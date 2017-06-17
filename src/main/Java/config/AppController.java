4c2d82e4fb90d896b0a539c62914cd1b1b3f5c64
sutiancheng <1020453479@qq.com>
2017-06-17 10:35:23 +0800

    第一次提交

---
+++ src/main/Java/config/AppController.java
@@ -0,0 +1,12 @@
+package config;
+
+import com.jfinal.core.Controller;
+
+/**
+ * Created by Administrator on 2017/6/17.
+ */
+public class AppController extends Controller {
+    public void index(){
+        renderText("»¶Ó­");
+    }
+}
---
+++ src/main/Java/config/DemoConfig.java
@@ -0,0 +1,33 @@
+package config;
+
+import com.jfinal.config.*;
+
+/**
+ * Created by Administrator on 2017/6/16.
+ */
+public class DemoConfig extends JFinalConfig {
+    @Override
+    public void configConstant(Constants constants) {
+
+    }
+
+    @Override
+    public void configRoute(Routes routes) {
+
+    }
+
+    @Override
+    public void configPlugin(Plugins plugins) {
+
+    }
+
+    @Override
+    public void configInterceptor(Interceptors interceptors) {
+
+    }
+
+    @Override
+    public void configHandler(Handlers handlers) {
+
+    }
+}
